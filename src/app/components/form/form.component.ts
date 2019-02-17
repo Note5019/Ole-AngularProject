6dcf6169a704695482f202a1c0ca75a806c83899
Note5019 <notenoppadon5019@gmail.com>
2019-02-17 11:24:07 +0700

    add form Detail

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
+  formGroup:FormGroup;
+  constructor(private fromBuild: FormBuilder) {
+   }
 
   ngOnInit() {
+    this.formGroup =  this.fromBuild.group({
+      firstName: this.fromBuild.control(''),      //แบบ เต็ม
+      lastName: ['']                              //แบบ ย่อ
+    });
   }
 
 }
